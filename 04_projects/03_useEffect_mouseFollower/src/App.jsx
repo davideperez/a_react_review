27c0ceb16496c8247c6e567d5500586c0f155a05
David <david.ezequiel.perez@gmail.com>
2024-07-12 15:05:09 -0300

    Basic useEffect mouse tracking app developed.

--- 04_projects/03_useEffect_mouseFollower/src/App.jsx
+++ 04_projects/03_useEffect_mouseFollower/src/App.jsx
@@ -1,8 +1,57 @@
+import { useEffect, useState } from 'react'
+
 function App () {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('Effect.', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+
+      console.log('handleMove: ', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // cleanup
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  const handleActivarSeguirPuntero = () => {
+    setEnabled(!enabled)
+  }
+
+  const buttonText = enabled ? 'Desactivar' : 'Activar'
   return (
-    <>
-      <h1>Proyecto 3</h1>
-    </>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button
+        onClick={handleActivarSeguirPuntero}
+      >
+        {buttonText} Seguir Puntero
+      </button>
+    </main>
   )
 }
 
