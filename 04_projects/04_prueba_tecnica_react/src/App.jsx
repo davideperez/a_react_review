f03c20d3f3c5cf2150bf0a579f2282e92dc83dad
David <david.ezequiel.perez@gmail.com>
2024-07-23 11:25:09 -0300

    The fetch done to the Facts API on the useEffect was modularized onto a custom function.

--- 04_projects/04_prueba_tecnica_react/src/App.jsx
+++ 04_projects/04_prueba_tecnica_react/src/App.jsx
@@ -4,42 +4,33 @@ import './App.css'
 const CAT_RANDOM_FACT_ENDPOINT = 'https://catfact.ninja/fact'
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/says'
 
-let acumulador = 0
-
-
 export function App () {
-
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
   // TODO: Error Handling.
   /*   const [factError, setFactError] = useState()
   const [imageError, setImageError] = useState() */
 
-  // Fetches the random fact API.
-  useEffect(()=> {
+  const getRandomFact = () => {
     fetch(CAT_RANDOM_FACT_ENDPOINT)
       .then(response => {
         // Error handler
         if (!response.ok) {
-          setFactError(`Fetching the ${CAT_PREFIX_FACT_URL} API was not possible.`)
+          // setFactError(`Fetching the ${CAT_PREFIX_FACT_URL} API was not possible.`)
         }
-        // 
+        //
         return response.json()
       })
       .then(data => {
         const { fact } = data
         setFact(fact)
       })
-          /* 
-      // Forma con async await
-      async function getRandomFact () {
-        const response = await fetch(CAT_RANDOM_FACT_ENDPOINT)
-        const data = await response.json()
-        setFact(data.fact)
-      }
-      getRandomFact()
- */
-  },[])
+  }
+
+  // Fetches the random fact API.
+  useEffect(() => {
+    getRandomFact()
+  }, [])
 
   // Fetches the Cat Images API
   useEffect(() => {
@@ -48,10 +39,10 @@ export function App () {
     const threeFirstWords = fact.split(' ', 3).join(' ')
     let url
 
-    fetch(`${CAT_PREFIX_IMAGE_URL}/${threeFirstWords}?fontSize=50&fontColor=red&json=true`) //${threeFirstWords}?size=50&color=red&json=true
+    fetch(`${CAT_PREFIX_IMAGE_URL}/${threeFirstWords}?fontSize=50&fontColor=red&json=true`) // ${threeFirstWords}?size=50&color=red&json=true
       .then(res => {
-/*
-        // TODO Handle Error if fact 
+      /*
+        // TODO Handle Error if fact
         if (!response.ok) {
           setImageError(`Fetching the ${CAT_PREFIX_IMAGE_URL} API was not possible.`)
         } */
@@ -59,21 +50,26 @@ export function App () {
         url = res.url
         url = url.replace('&json=true', '')
         setImageUrl(url)
-/*       }).catch(err => {
+      /*       }).catch(err => {
         console.log(err)
       }) */
+      })
   }, [])
-  })
+
+  const handleClick = () => {
+    getRandomFact()
+  }
+
   return (
     <main>
       <h1>App de gatitos!</h1>
+      <button onClick={handleClick}>Actualizar</button>
       {
-        imageUrl ? 
-          <img src={`${imageUrl}`} alt={`Image extracted using the three first words of ${fact}`}/>
-          :
-          <p>⚠️ La API de Gatitos no estaria funcionando, chequeala en: <a href={CAT_PREFIX_IMAGE_URL}>{CAT_PREFIX_IMAGE_URL}</a></p>
+        imageUrl
+          ? <img src={`${imageUrl}`} alt={`Image extracted using the three first words of ${fact}`} />
+          : <p>⚠️ La API de Imagenes de Gatitos no estaria funcionando, chequeala en: <a href={CAT_PREFIX_IMAGE_URL}>{CAT_PREFIX_IMAGE_URL}</a></p>
       }
-      { fact && <p>{fact}</p> }
+      {fact && <p>{fact}</p>}
     </main>
   )
 }
