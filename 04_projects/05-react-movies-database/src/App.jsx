161f46fcfbdbaf116368bec2a69131ad4b8bc794
David <david.ezequiel.perez@gmail.com>
2024-07-30 19:25:12 -0300

    05-react-movies-database initial commit.

---
+++ 04_projects/05-react-movies-database/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App () {
+
+  return (
+    <>
+    <main 
+      style={{
+        'padding' : '50px',
+      }}
+    >
+      <h1>Titulo</h1>
+      <p>Buenas</p>
+      <button>Click Aqui</button>
+    </main>
+    </>
+  )
+}
+
+export default App
