10ea66d931591aadc9f2a31aac9a64c730c8bc33
David <david.ezequiel.perez@gmail.com>
2024-07-31 02:42:54 -0300

    Form created. Movies are listed with a mock response.

--- 04_projects/05-react-movies-database/src/App.jsx
+++ 04_projects/05-react-movies-database/src/App.jsx
@@ -1,19 +1,40 @@
 import './App.css'
+import responseMovies from '../mock/with-results.json'
+import withoutResultsMovies from '../mock/no-results.json'
 
 function App () {
+  const movies = responseMovies.Search
+  console.log(movies)
+  const hasMovies = movies?.length > 0
 
   return (
-    <>
-    <main 
-      style={{
-        'padding' : '50px',
-      }}
-    >
-      <h1>Titulo</h1>
-      <p>Buenas</p>
-      <button>Click Aqui</button>
-    </main>
-    </>
+    <div className='page'>
+      <header>
+        <h1>Movies Database</h1>
+        <form className='form'>
+          <input placeholder='Avengers, The Matrix, Star Wars... '/> 
+          <button type='submit'>Search</button>
+        </form>
+      </header>
+      <main>
+        {
+          hasMovies 
+            ? (
+              <ul>
+                {
+                  movies.map(movie => (
+                    <li key={movie.imdbID}>
+                      <h3>{movie.Title} ({movie.Year})</h3>
+                      <img src={movie.Poster} alt={movie.Title}/>
+                    </li>
+                  ))
+                }
+              </ul>
+            )
+            : <p>There are no movies with this search.</p>
+        }
+      </main>
+    </div>
   )
 }
 
