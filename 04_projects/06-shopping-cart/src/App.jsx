030194effdceb3784115d3fcf3e863a66256ac6c
David <david.ezequiel.perez@gmail.com>
2024-08-15 04:18:47 -0300

    Icons added. Products mock added. Products component already showing the products.

--- 04_projects/06-shopping-cart/src/App.jsx
+++ 04_projects/06-shopping-cart/src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css'
+import { useState } from 'react'
+import Products from './components/Products'
+// import { getProducts, getProducts } from './services/getProducts'
+import { products as initialProducts } from './mock/products.json'
 
 function App () {
+  const [products] = useState(initialProducts)
+
   return (
     <>
       <h1>Shopping Cart: 🛒</h1>
+      <Products products={products} />
     </>
   )
 }
