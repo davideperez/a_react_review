41a64504b9ce8a66bb52fc0d872ab9ce41720070
David <david.ezequiel.perez@gmail.com>
2024-08-16 00:00:30 -0300

    Filter logic implemented as an example of prop drilling. It will be corrected.

--- 04_projects/06-shopping-cart/src/components/Filters.jsx
+++ 04_projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,13 +1,40 @@
 import { useState } from 'react'
 import './Filters.css'
 
-export function Filters () {
-  const [minPrice, setMinPrice] = useState()
+export function Filters ({ onChange }) {
+  const [minPrice, setMinPrice] = useState(0)
+  const [category, setCategory] = useState('all')
 
   const handleChangeMinPrice = (event) => {
     const selectedMinPrice = event.target.value
     setMinPrice(selectedMinPrice)
+    // Esto huele mal.
+    // Porque con el onChange, estamos pasando la funcion de actualizar estado..
+    // ..nativa de react, a un componente hijo.
+    onChange(prevState => {
+      console.log('prevState: ', prevState)
+      return {
+        ...prevState,
+        minPrice: selectedMinPrice
+      }
+    })
   }
+
+  const handleChangeCategory = (event) => {
+    const selectedCategory = event.target.value
+    setCategory(selectedCategory)
+    // Esto huele mal.
+    // Porque con el onChange, estamos pasando la funcion de actualizar estado..
+    // ..nativa de react, a un componente hijo.
+    onChange(prevState => {
+      console.log('prevState: ', prevState)
+      return {
+        ...prevState,
+        category: selectedCategory
+      }
+    })
+  }
+
   return (
     <section className='filters'>
       <div>
@@ -16,14 +43,14 @@ export function Filters () {
           type='range'
           id='price'
           min='0'
-          max='2000'
+          max='1000'
           onChange={handleChangeMinPrice}
         />
         <span>${minPrice}</span>
       </div>
       <div>
         <label htmlFor='category'>Category</label>
-        <select id='category'>
+        <select id='category' onChange={handleChangeCategory}>
           <option value='all'>All</option>
           <option value='beauty'>Beauty</option>
           <option value='furniture'>Furniture</option>
