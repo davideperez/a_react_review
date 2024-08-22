2163978018b9089f177398ca3b62537f35dff5b7
David <david.ezequiel.perez@gmail.com>
2024-08-22 03:43:03 -0300

    Filename correction.

--- 04_projects/06-shopping-cart/src/main.jsx
+++ 04_projects/06-shopping-cart/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { FiltersProvider } from './context/filters.jsx'
+import { FiltersProvider } from './context/filters-context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <FiltersProvider>
