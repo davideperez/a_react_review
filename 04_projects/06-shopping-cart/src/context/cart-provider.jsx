c28acfa11a2db3b1c79486f9d06896e3cd530267
David <david.ezequiel.perez@gmail.com>
2024-08-22 19:41:40 -0300

    Modularized the context from the provider

---
+++ 04_projects/06-shopping-cart/src/context/cart-provider.jsx
@@ -0,0 +1,21 @@
+import { useCartReducer } from '../hooks/useCartReducer'
+import { CartContext } from './cart-context'
+
+// 2. Crear Provider
+// La dependencia de usar React Context, es MINIMA!
+export function CartProvider ({ children }) {
+  const { addToCart, removeItemFromCart, clearCart, cart } = useCartReducer()
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeItemFromCart,
+        clearCart
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
