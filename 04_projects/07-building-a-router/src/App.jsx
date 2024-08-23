81cb5f3a7778641545e48e08bfbf7c5487fddf6a
David <david.ezequiel.perez@gmail.com>
2024-08-23 02:12:28 -0300

    Multiple Page Application with a rudimental conditional rendering.

--- 04_projects/07-building-a-router/src/App.jsx
+++ 04_projects/07-building-a-router/src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from 'react'
 import './App.css'
 
-function App () {
+function HomePage () {
   return (
     <>
-      <h1>Building a Router</h1>
+      <h1>Home</h1>
+      <p>Pagina de ejemplo para crear React Router desde cero.</p>
+      <a href='/about'>Sobre Nosotros</a>
     </>
+  )
+}
+
+function AboutPage () {
+  return (
+    <>
+      <h1>About</h1>
+      <div>
+        <img src='https://images.pexels.com/photos/192136/pexels-photo-192136.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='Foto de un bosque.' />
+        <p>Hola me llamo David y estoy creando un Router desde cero.</p>
+
+      </div>
+      <a href='/'>Ir a la Home</a>
+    </>
+  )
+}
+
+function App () {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  return (
+    <main>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
   )
 }
 
