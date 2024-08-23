9bb2ec50a9d8a50077708dd6c2ccc741b610c22b
David <david.ezequiel.perez@gmail.com>
2024-08-23 16:14:31 -0300

    Router logic modularized.

--- 04_projects/07-building-a-router/src/App.jsx
+++ 04_projects/07-building-a-router/src/App.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import { EVENTS } from './consts'
 import HomePage from './pages/Home'
 import AboutPage from './pages/About'
+
+import { Router } from './Router'
 
 /*
   Button not being an anchor, have accesibility issues.
@@ -20,28 +20,6 @@ const routes = [
     Component: AboutPage
   }
 ]
-
-function Router ({ routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1> }) {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname)
-
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPath(window.location.pathname)
-    }
-
-    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
-    window.addEventListener(EVENTS.POPSTATE, onLocationChange)
-
-    return () => {
-      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
-      window.removeEventListener(EVENTS.POPSTATE, onLocationChange)
-    }
-  }, [])
-
-  const Page = routes.find(({ path }) => path === currentPath)?.Component
-
-  return Page ? <Page /> : <DefaultComponent />
-}
 
 function App () {
   return (
